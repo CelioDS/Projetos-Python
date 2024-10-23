15789751ee1de7e465848f23bcda12306035a2a7
Célio Da Silva <106108913+CelioDS@users.noreply.github.com>
2024-10-23 00:47:13 -0300

    FEAT: new exercises

--- dio-bootcamp/DADOS/default_archive.py
+++
@@ -1,5 +0,0 @@
-import random
-
-branch = random.sample(range(0, 9), 5)
-
-print(branch)
--- dio-bootcamp/bank.py
+++ dio-bootcamp/bank.py
@@ -12,6 +12,10 @@ blue = "\033[94m"
 criar clientes e fazer verificação
 mudar menu para primeiro menu com criar conta e acessar contard
 depois que estiver na conta mostrar os menus de acções
+deixar cpf unico validar
+validar numero de conta unico
+
+para vincular a conta com o cliente pelo cpf
 
 
 '''
@@ -20,6 +24,10 @@ def run_test():
 
 def get_current_time():
     return datetime.now().strftime("%H:%M-%d/%m/%Y")
+
+def mask_cpf(cpf):
+    mask = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+    return mask
 
 class Bank:
     def __init__(self, initial_balance=0.00):
@@ -32,7 +40,8 @@ class Bank:
       self.historic_account = list()
       self.historic_transactions = []
       self.account_default = '0001'
-      self.account_user = []
+      self.account_users = []
+      self.user_now = []
 
 
     def historic_transaction_log(self,func):
@@ -54,11 +63,20 @@ class Bank:
             print("cpf invalid")
             cpf = str(input("Cpf:"))
 
-        cpf_mask = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+        cpf_default = mask_cpf(cpf)
+
         branch_mask = f"{branch[:4]}-{branch[4:]}"
 
-        self.account_user.append((name, cpf_mask, branch_mask, self.historic_account) )
-        print(self.account_user)
+        self.account_users.append((name, cpf_default, branch_mask, {'Balance': 0},[] ) )
+        print(self.account_users)
+
+    def login_account(self, cpf):
+        cpf_default = mask_cpf(cpf)
+
+        for account_user in self.account_users:
+            if account_user[1] == cpf_default:
+                print(f"user: {account_user[0]}")
+                self.user_now = account_user
 
 
     def withdraw(self, withdraw_value=None):
@@ -77,22 +95,24 @@ class Bank:
                 print(f"-\n{red}value negative{white}")
                 withdraw_value = float(input("\ninform the withdrawal amount:\nR$:"))
 
-            if withdraw_value > self.balance:
-                print(f'\n{red}-insufficient balance{white}')
-                print(f"💳Balance: R$:{self.balance}")
-                return
-
             if withdraw_value <= self.limit['value_limit'] and self.limit['qtd'] > 0:
 
                 self.limit["qtd"] -= 1
                 self.limit['transaction'] -=1
+
                 if self.limit["qtd"] == 3:
                     print(f"\n-its daily value is {green}{self.limit['value_limit']*3}{white} and the withdrawal limit is {green}{self.limit['qtd']}{white} ")
 
                 print(f"\n-withdraw R$:{withdraw_value} loading....")
 
 
-                if withdraw_value <= self.balance:
+                if withdraw_value != self.balance:
+                    'Corrigir depois'
+
+                    for user in self.account_users:
+                        if self.user_now == user:
+                            self.user_now[3]["Balance"] -= withdraw_value
+                            self.user_now[4].append(("WITHDRAW", withdraw_value, get_current_time()))
 
                     self.balance -= withdraw_value
                     self.historic_account.append(("WITHDRAW", withdraw_value, get_current_time()))
@@ -111,7 +131,7 @@ class Bank:
 
             print(f"\n-{red}entry incorrect,please, enter a valid value{white}")
 
-        print("".center(50, ' '))
+        print("".center(53, ' '))
         time.sleep(0.5)
 
     def deposit(self, deposit_value=None):
@@ -130,21 +150,22 @@ class Bank:
 
             print(f"\n-deposit  loading....")
             time.sleep(0.5)
-            self.balance += deposit_value
-            self.historic_account.append(("DEPOSIT", deposit_value, get_current_time()))
+
+            for user in self.account_users:
+                if self.user_now == user:
+                    self.user_now[3]["Balance"] += deposit_value
+                    self.user_now[4].append(("DEPOSIT", deposit_value, get_current_time()))
+
             self.limit['transaction'] -= 1
 
             print(f"-deposit  success of {green}{deposit_value}{white} equal {blue}{self.balance}{white}....")
 
-
-
-
-
+            print(self.account_users)
         except ValueError:
 
             print(f"\n-{red}entry incorrect,please, enter a valid value{white}")
 
-        print("".center(50, ' '))
+        print("".center(53, ' '))
         time.sleep(0.5)
 
     def extract(self):
@@ -152,28 +173,36 @@ class Bank:
         print(f"""\n-💳Balance R$:{blue}{self.balance:.2f}{white}\n
 here is your transaction history""")
 
-        print("EXTRACT".center(50, '-'))
+        print("EXTRACT".center(53, '-'))
         if len(self.historic_account) == 0:
             print("-No extract")
-        for transaction, value, date in self.historic_account:
-            print(f"{transaction:<15} R$ {value:<10}{date:<30}")
+        for user in self.account_users:
+            if self.user_now == user:
+                for transaction, value, date in self.user_now[4]:
+                    if transaction == "WITHDRAW":
+                        value_display = f"{red}{value:.2f}{white}"
+                    else:
+                        value_display = f"{green}{value:.2f}{white}"
+                    print(f"{transaction:<15} R$:{value_display:<25} {date:<30}")
 
 
         time.sleep(3)
+
 account = Bank(initial_balance=0)
 
 if __name__ == "__main__":
     while True:
-        print("option menu".center(50, '-'))
+        print("option menu".center(53, '-'))
         print("""
         [1] withdraw 💸
         [2] Deposit  💵
         [3] Extract  📄
         [4] Exit     ❌
         [5] TEST     🤖
-        [6] create account
+        [6] singing
+        [7] login - teste cpf
         """)
-        print("".center(50,'-'))
+        print("".center(53,'-'))
 
         try:
             option = int(input("choose your option:\n>>>"))
@@ -190,7 +219,19 @@ if __name__ == "__main__":
             elif option == 5:
                 run_test()
             elif option == 6:
-                account.create_account()
+                account.create_account('celio','50816832803')
+                account.create_account("eu", '50816832804')
+            elif option == 7:
+                account.login_account('50816832803')
+                account.deposit(480000)
+                account.withdraw(100)
+                account.extract()
+            elif option == 8:
+                account.login_account('50816832804')
+                account.deposit(480000)
+                account.withdraw(100)
+                account.extract()
+
             else:
                 print(f"\n{red}-option invalid{white}")
 
